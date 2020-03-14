2104e6cbcec31685e5e62bc9d67f14e3a7d04903
Benny Hinrichs <hinrichs.benny@gmail.com>
2020-03-14 15:28:10 -0600

    Updates

---
+++ precache-manifest.7c8b3793a744ff15c1c364be726426ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd86b2388a1ec509bda0eff4084faee",
+    "url": "/react-product-gallery/index.html"
+  },
+  {
+    "revision": "987d3be4c47d9489ff0f",
+    "url": "/react-product-gallery/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "bbb7beab1be78abefaf3",
+    "url": "/react-product-gallery/static/css/main.9510a3e0.chunk.css"
+  },
+  {
+    "revision": "987d3be4c47d9489ff0f",
+    "url": "/react-product-gallery/static/js/2.1283d299.chunk.js"
+  },
+  {
+    "revision": "bbb7beab1be78abefaf3",
+    "url": "/react-product-gallery/static/js/main.8dd0d08d.chunk.js"
+  },
+  {
+    "revision": "aa6255373ea226594cd1",
+    "url": "/react-product-gallery/static/js/runtime~main.ecb2a0e0.js"
+  }
+]);
